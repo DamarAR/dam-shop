6cff8a9e6ad592bdbac3699fde49d0cf8fdaec20
DamarAR <aryaputra.damar@gmail.com>
2024-09-12 21:55:10 +0700

    updating assignment 3

--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+import uuid  
 
 # Create your models here.
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.IntegerField()
+class FoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # add this line
+    foods = models.CharField(max_length=255)
+    time = models.DateField(auto_now_add=True)
     description = models.TextField()
-    
+    quantity = models.IntegerField()
